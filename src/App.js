b305f76f2113d67595a72c10f89222d7e4044e93
Reshi <juancdominici@gmail.com>
2020-11-02 16:46:14 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      break: 5,
+      session: 25,
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title id="break-label">Break length</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <ButtonGroup>
+                  <Button
+                    variant="warning"
+                    id="break-increment"
+                    onClick={this.handleIncrement}
+                  >
+                    ↑
+                  </Button>
+                  <Button variant="warning" disabled>
+                    5
+                  </Button>
+                  <Button
+                    variant="warning"
+                    id="break-decrement"
+                    onClick={this.handleDecrement}
+                  >
+                    ↓
+                  </Button>
+                </ButtonGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title id="session-label">Session length</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <ButtonGroup>
+                  <Button
+                    variant="info"
+                    id="session-increment"
+                    onClick={this.handleIncrement}
+                  >
+                    ↑
+                  </Button>
+                  <Button variant="info" disabled>
+                    25
+                  </Button>
+                  <Button
+                    variant="info"
+                    id="session-decrement"
+                    onClick={this.handleDecrement}
+                  >
+                    ↓
+                  </Button>
+                </ButtonGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title id="timer-label">Session</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text id="time-left">
+                
+                </Card.Text>
+                <ButtonGroup>
+                  <Button
+                    variant="secondary"
+                    id="start__stop"
+                  >
+                    ►||
+                  </Button>
+                  <Button variant="secondary" id="reset">
+                    ♺
+                  </Button>
+                </ButtonGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>{" "}
+      </Container>
+    );
+  }
 }
+
 
 export default App;
